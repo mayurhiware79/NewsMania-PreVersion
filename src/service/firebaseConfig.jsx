9d0a3b1ca5be24aaa355087358098847eac1505a
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-04-12 21:45:00 +0530

    Update firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDy88DienRW5QIUIEKSTzndKoIqxvZxD4A",
+  apiKey: "firebase abi key",
   authDomain: "ai-trip-planner-ae60d.firebaseapp.com",
   projectId: "ai-trip-planner-ae60d",
   storageBucket: "ai-trip-planner-ae60d.firebasestorage.app",
